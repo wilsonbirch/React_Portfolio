ccbcf927a6504a18b5dbed7577bc8480a6e53017
Wilson Birch <wilsonbirch@gmail.com>
2020-10-15 16:46:56 -0400

    Updates

---
+++ precache-manifest.06ad08a7ccfa52a15876a6a472ac9873.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b073cd8efd0a9abd798f897734f057",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "6ab4485526b9753e9a67",
+    "url": "/React_Portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "bcb4561b6265f35c56ec",
+    "url": "/React_Portfolio/static/css/main.8e2e798d.chunk.css"
+  },
+  {
+    "revision": "6ab4485526b9753e9a67",
+    "url": "/React_Portfolio/static/js/2.57f1d971.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/React_Portfolio/static/js/2.57f1d971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcb4561b6265f35c56ec",
+    "url": "/React_Portfolio/static/js/main.806f44ea.chunk.js"
+  },
+  {
+    "revision": "8ce534014186303edb41",
+    "url": "/React_Portfolio/static/js/runtime-main.b3be291f.js"
+  },
+  {
+    "revision": "3c8cb700e7a21b974ab9eea505b055e1",
+    "url": "/React_Portfolio/static/media/GitHub.3c8cb700.png"
+  },
+  {
+    "revision": "3fbdcb308f10a16b0babb94ef52c7ddf",
+    "url": "/React_Portfolio/static/media/PW_generator.3fbdcb30.png"
+  },
+  {
+    "revision": "0142eb5aac02000a6f85cdfaf9a8bf88",
+    "url": "/React_Portfolio/static/media/TM_screenshot_.0142eb5a.png"
+  },
+  {
+    "revision": "0ccdbf9615a404d55e49b94c36d803e5",
+    "url": "/React_Portfolio/static/media/Wilson_Birch_Resume.0ccdbf96.pdf"
+  },
+  {
+    "revision": "4edf5b1265147c6575b70856b8a7a5a0",
+    "url": "/React_Portfolio/static/media/Wilson_Panda.4edf5b12.png"
+  },
+  {
+    "revision": "f17e6f109cd9aa1cc80d45102c62b8a7",
+    "url": "/React_Portfolio/static/media/atnmy_screenshot.f17e6f10.png"
+  },
+  {
+    "revision": "7b46f6b32c695977db660ed4659857eb",
+    "url": "/React_Portfolio/static/media/burger_tracker_screenshot.7b46f6b3.png"
+  },
+  {
+    "revision": "5d130677e1ac05a11e03479ab5c91635",
+    "url": "/React_Portfolio/static/media/emp_track_screenshot.5d130677.png"
+  },
+  {
+    "revision": "c5502bbb9b856c227ec3ec95021a7c46",
+    "url": "/React_Portfolio/static/media/employee_template_screenshot.c5502bbb.png"
+  },
+  {
+    "revision": "18e2b5a8c6bd2752a79008fc2234390f",
+    "url": "/React_Portfolio/static/media/project_1.18e2b5a8.png"
+  },
+  {
+    "revision": "f57422af63fe9627378d1c32752c3b7d",
+    "url": "/React_Portfolio/static/media/react_employee.f57422af.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React_Portfolio/static/media/react_logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a362a48d70a038f35d232659bfb4c28f",
+    "url": "/React_Portfolio/static/media/rest_reviewer_screenshot.a362a48d.png"
+  },
+  {
+    "revision": "bd3452b7b4bc31da90c38a80a7fbd3c7",
+    "url": "/React_Portfolio/static/media/workout_tracker.bd3452b7.png"
+  }
+]);
