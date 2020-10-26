3017e73280caad14b78068760ac4853703532f4b
Wilson Birch <wilsonbirch@gmail.com>
2020-10-26 15:25:33 -0400

    Updates

---
+++ precache-manifest.65b799974db256ed0cc5c4c3e55fc8b2.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d19f5efbe637ef20133b81e71cc1017",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f94a265aa764e2e44aa",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ed1a770c6656410003c8",
+    "url": "/static/css/main.8e2e798d.chunk.css"
+  },
+  {
+    "revision": "4f94a265aa764e2e44aa",
+    "url": "/static/js/2.9046db8c.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.9046db8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed1a770c6656410003c8",
+    "url": "/static/js/main.e5563eea.chunk.js"
+  },
+  {
+    "revision": "a36784af4abdca6cc163",
+    "url": "/static/js/runtime-main.ea1ad2b7.js"
+  },
+  {
+    "revision": "3c8cb700e7a21b974ab9eea505b055e1",
+    "url": "/static/media/GitHub.3c8cb700.png"
+  },
+  {
+    "revision": "3fbdcb308f10a16b0babb94ef52c7ddf",
+    "url": "/static/media/PW_generator.3fbdcb30.png"
+  },
+  {
+    "revision": "0142eb5aac02000a6f85cdfaf9a8bf88",
+    "url": "/static/media/TM_screenshot_.0142eb5a.png"
+  },
+  {
+    "revision": "0ccdbf9615a404d55e49b94c36d803e5",
+    "url": "/static/media/Wilson_Birch_Resume.0ccdbf96.pdf"
+  },
+  {
+    "revision": "4edf5b1265147c6575b70856b8a7a5a0",
+    "url": "/static/media/Wilson_Panda.4edf5b12.png"
+  },
+  {
+    "revision": "f17e6f109cd9aa1cc80d45102c62b8a7",
+    "url": "/static/media/atnmy_screenshot.f17e6f10.png"
+  },
+  {
+    "revision": "7b46f6b32c695977db660ed4659857eb",
+    "url": "/static/media/burger_tracker_screenshot.7b46f6b3.png"
+  },
+  {
+    "revision": "5d130677e1ac05a11e03479ab5c91635",
+    "url": "/static/media/emp_track_screenshot.5d130677.png"
+  },
+  {
+    "revision": "c5502bbb9b856c227ec3ec95021a7c46",
+    "url": "/static/media/employee_template_screenshot.c5502bbb.png"
+  },
+  {
+    "revision": "41f5ae20c7a5484be94e085eb57c1e83",
+    "url": "/static/media/googlebooks.41f5ae20.png"
+  },
+  {
+    "revision": "18e2b5a8c6bd2752a79008fc2234390f",
+    "url": "/static/media/project_1.18e2b5a8.png"
+  },
+  {
+    "revision": "f57422af63fe9627378d1c32752c3b7d",
+    "url": "/static/media/react_employee.f57422af.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/react_logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a362a48d70a038f35d232659bfb4c28f",
+    "url": "/static/media/rest_reviewer_screenshot.a362a48d.png"
+  },
+  {
+    "revision": "bd3452b7b4bc31da90c38a80a7fbd3c7",
+    "url": "/static/media/workout_tracker.bd3452b7.png"
+  }
+]);
