6830d15c8fe064bea2b745f31969568a25bb9c1e
Wilson Birch <wilsonbirch@gmail.com>
2021-07-30 08:22:57 -0700

    portfolio updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import About from './components/pages/About';
+import Portfolio from './components/pages/Portfolio';
+import Contact from './components/pages/Contact';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Header />
+			<Switch >
+				<Route exact path='/' component={About} />
+				<Route exact path='/portfolio' component={Portfolio} />
+				<Route exact path='/contact' component={Contact} />
+			</Switch>
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Contact (props) {
+
+  return (
+  
+      <main className="container">
+    
+        <article className="row">
+      
+          <section className="card contactCard">
+    
+            <h5 className="card-header" >Contact</h5>
+    
+            <section className="card-body">
+
+            <a className="btn btn-dark" href="mailto:wilsonbirch@gmail.com" >Email</a>
+    
+            </section>
+          </section>
+        </article>
+      </main>
+  );
+  }
+  
+  export default Contact;
